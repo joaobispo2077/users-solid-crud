416c4dd6bcbfe5183ec62de92feead6544eaf381
João Bispo <joaobispo2077@gmail.com>
2021-09-04 16:38:02 -0300

    feat: connect controller into use cases

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+      return response.status(200).json(user);
+    } catch (err) {
+      return response.status(404).json({
+        error: err.message || "Unexpected error.",
+      });
+    }
   }
 }
 
