9a5999cdee309d7e32465085256c8d468c331e55
João Bispo <joaobispo2077@gmail.com>
2021-10-31 10:21:07 -0300

    docs: add list users, create user and get one user into doc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 export { app };
